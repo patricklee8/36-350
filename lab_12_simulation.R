ead1a072620a02f573169192ffff445ef0009909
patricklee8 <patricklee8@gmail.com>
2018-04-19 21:43:08 -0400

    Question 2d

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,8 +3,13 @@ run_simulation=function(n_trials, n, p, cutoff){
   for (n in (1:n_trials)){
   for (i in c(10,100,1000)){
     for (j in c(10,20,50)){
-  hist(model_select(generate_data(j,i)$covariates,generate_data(n_trials,i)$responses,0.05)$pvalues.2)
+  save(data=model_select(generate_data(j,i)$covariates,generate_data(n_trials,i)$responses,0.05)$pvalues.2, file="datapath")
     }
   }
   }
+}
+
+make_plot=function(datapath){
+  load("datapath")
+  hist(data)
 }
