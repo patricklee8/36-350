fb78cbd686a310c8aad48f6b59159de3a21fda71
patricklee8 <patricklee8@gmail.com>
2018-04-19 21:09:25 -0400

    Question 2b

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,3 +1,12 @@
-generate_data=function(n,p){
-return(list(covariates=matrix(nrow=n,ncol=p)
+
+model_select=function(covariates,responses,cutoff){
+  lin.reg=lm(responses~covariates)
+  testresult=t.test(lin.reg)[[3]]
+  wcutoff=lin.reg[testresult<=cutoff]
+  
+  linreg.2=lm(responses ~ covariates[testresult<=cutoff,])
+  pvalues.2=t.test(linreg.2)[[3]]
+  return(wcutoff,pvalues.2)
 }
+
+
